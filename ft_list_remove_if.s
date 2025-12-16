ef0f732f1e49657af2a9158f76203793007b2ed0
diegosanchezstrange <diegosanchezstrangejavax@gmail.com>
2025-12-16 13:31:29 +0000

    Changin first element

--- ft_list_remove_if.s
+++ ft_list_remove_if.s
@@ -29,6 +29,18 @@ ft_list_remove_if:
     mov r13, rdx ; int (*cmp)()
     mov r14, rcx ; void (*free_fct)()
 
+    test rbx, rbx
+    jz .done
+
+    test r12, r12
+    jz .done
+
+    test r13, r13
+    jz .done
+
+    test r14, r14
+    jz .done
+
 .set_vars:
     mov rcx, [rbx]
     xor r15, r15 ; This will store the last element
@@ -37,24 +49,28 @@ ft_list_remove_if:
     test rcx, rcx
     jz .done
 
-    mov rdi, [rcx + 0]
+    mov rdi, [rcx + LIST_DATA] ; Load the data of the current element
     mov rsi, r12
 
+    push rcx
     call r13 ; Call the comparison function
+    pop rcx
 
-    test rax, rax
+    test rax, rax ; If diference is 0, we need to remove the element
     jz .remove
 
-    mov r15, rcx
+    mov r15, rcx ; Store the last element
     mov rcx, [rcx + LIST_NEXT]
     jmp .loop
 
 .remove:
-    mov rax, [rcx + LIST_NEXT] ; Save the next element
+    
+    mov rax, [rcx + LIST_NEXT] ; Save the next element SIGSEGV
 
     test r15, r15
     jz .first_element
 
+    ; Link the previous element to the next element
     mov [r15 + LIST_NEXT], rax
 
 .first_element:
@@ -62,23 +78,28 @@ ft_list_remove_if:
     ;call free for the element at rcx
     ;free_fct(rcx->data)
     push rax
-    push r15
-    mov rdi, [rcx + 0]
+    mov rdi, [rcx + LIST_DATA]
     push rcx
     call r14 ; Call the free function
     pop rcx
-    pop r15
     pop rax
 
     ;call free for the element at rcx
     ;free(rcx)
     push rax
-    push r15
     mov rdi, rcx
     call free wrt ..plt ; Call the free function
-    pop r15
     pop rax
 
+    ; Save the new first element if we removed the first first_element
+    test r15, r15
+    jz .update_head
+
+    mov rcx, rax
+    jmp .loop
+
+.update_head:
+    mov [rbx], rax
     mov rcx, rax
     jmp .loop
 
